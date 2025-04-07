fad4e6b45d4fd6338d065a32103cf0d4db99314d
williandeitosi <williandeitosi@gmail.com>
2025-04-07 16:22:14 -0300

    feat (api): create routes do list all and get one user

--- apps/api/src/user/routes/index.ts
+++ apps/api/src/user/routes/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
-import { register } from "../controllers";
+import { findByEmail, listAllUsers, register } from "../controllers";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/user/", register);
+  app.post("/user", register);
+  app.get("/user/all", listAllUsers);
+  app.post("/teste", findByEmail);
 }
