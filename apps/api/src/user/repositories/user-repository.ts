8af6547967a6121965da23ebf6db790cd952c576
williandeitosi <williandeitosi@gmail.com>
2025-04-07 16:21:12 -0300

    feat (api): feat: create interface list all users

--- apps/api/src/user/repositories/user-repository.ts
+++ apps/api/src/user/repositories/user-repository.ts
@@ -2,4 +2,5 @@ import type { Prisma, User } from "../../../generated/prisma";
 export interface UserRepository {
   findByEmail(email: string): Promise<User | null>;
   create(data: Prisma.UserCreateInput): Promise<User>;
+  listAll(): Promise<User[]>;
 }
