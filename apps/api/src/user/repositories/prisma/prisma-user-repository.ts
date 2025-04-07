c2dfae07fb478310f166fcbeaddd87fd4ba3f88d
williandeitosi <williandeitosi@gmail.com>
2025-04-07 12:25:22 -0300

    feat (api): create prisma user repository

---
+++ apps/api/src/user/repositories/prisma/prisma-user-repository.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../prisma/db";
+import type { UserRepository } from "../user-repository";
+import { Prisma } from "./../../../../generated/prisma/index.d";
+
+export class PrismaUserRepository implements UserRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    return user;
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+}
